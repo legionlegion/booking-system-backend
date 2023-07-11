d2a511f8f5c9175b1df965c1ae86626d40c76ddc
shuyuan <crayy.0803@gmail.com>
2023-07-11 16:56:29 +0800

    added gitignore and Procfile

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,10 +6,10 @@ import (
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://syal-2ae9b.firebaseapp.com")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization, Username")
 			return
