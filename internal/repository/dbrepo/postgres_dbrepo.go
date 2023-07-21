0d8969f81a207aa5541682025372a97092e5cc0c
shuyuan <crayy.0803@gmail.com>
2023-07-22 00:30:26 +0800

    Fixed logic of AllBookings function to not filter by 2 weeks

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -30,9 +30,6 @@ func (m *PostgresDBRepo) AllBookings() ([]*models.SubmittedBooking, error) {
 		start_time, end_time, purpose, facility 
 	from 
 		approvedbookings 
-	where 
-		start_time >= date_trunc('week', current_date) 
-		and end_time < date_trunc('week', current_date) + interval '2 weeks'
 	order by 
 		id
 	`
